d02ee4e8f9f218b0314fa819deeda9233c8cfe3c
MasaMesgho <james_sam_whittingham@outlook.com>
2026-02-27 18:18:23 +0000

    test scene
    test scene created and draw/discard objects added.
    control script being worked on

---
+++ Main_HUD.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var draw_window: Control = $DrawWindow
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_draw_button_pressed() -> void:
+	print("Button Pressed")
+	if draw_window.visible:
+		draw_window.visabile = false
+	else:
+		draw_window.visibile = true
+	pass # Replace with function body.
