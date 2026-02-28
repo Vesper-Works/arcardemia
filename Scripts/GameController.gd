a384aa4966d62a2648229e0ad9848552d1d6305f
mindlyanarch <29086791@students.lincoln.ac.uk>
2026-02-28 20:36:20 +0000

    a

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -1,9 +1,12 @@
 class_name GameController
 extends Node
 
+@onready var state: String = "Cutscene"
 @onready var PillarAnimator: AnimationPlayer = %BoxAnimator
 @onready var CameraAnimator: AnimationPlayer = %PlayerAnimator
 @onready var Loot: Control = %Loot
+@onready var CardController: Control = %CardController
+@onready var player_deck: Deck = CardController.player_deck
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	CameraAnimator.play("ZoomOnBox")
@@ -21,10 +24,32 @@ func LootCutsceneStart():
 
 	
 func LootPhase():
-	var Option1: Augment = Loot.get_augment()
+	state = "Loot"
+	#pick a card from the deck
 	
-	var Option2: Card = Loot.get_basic_card()
-	var Option3: Relic = Loot.get_relic()
+	var deckcard1: Card = null
+	var deckcard2: Card = null
+	
+	if !player_deck.has_basic():
+		deckcard1 = Loot.get_basic_card()
+	else:
+		deckcard1 = player_deck.get_basic()
+
+	if !player_deck.has_basic():
+		deckcard2 = Loot.get_basic_card()
+	else:
+		deckcard2= player_deck.get_basic()
+		
+	#set rewards
+	var Reward1: Card = Loot.get_augment(deckcard1)
+	var Reward2: Card = Loot.get_augment(deckcard2)
+	var Reward3: Card = Loot.get_basic_card()
+	
+	%Option1Card.style_to_card(Reward1)
+	%Option2Card.style_to_card(Reward2)
+	%Option3Card.style_to_card(Reward3)
+	
+
 	
 	
 
