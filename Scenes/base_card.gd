4ec44f17fbacc2eb25050d27915b6b69594a9586
Vesper-Works <59376295+Vesper-Works@users.noreply.github.com>
2026-02-28 19:37:59 +0000

    Added button logic

--- Scenes/base_card.gd
+++ Scenes/base_card.gd
@@ -1,10 +1,14 @@
 extends Panel
 
+signal on_played
+
 @onready var card_title: Label = %CardTitle
 @onready var card_art: TextureRect = %CardArt
 @onready var card_description: Label = %CardDescription
 @onready var card_suit: Label = %CardSuit
 
+var click_start_time : int = 0
+var initial_mouse_offset : Vector2
 func style_to_card(card : Card):
 	card_title.text = card.name
 	card_description.text = card.description
@@ -20,3 +24,26 @@ func style_to_card(card : Card):
 		Card.CardSuit.none:
 			card_suit.text = ""	
 	pass
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	
+	if event is InputEventMouse and click_start_time > 0:
+		#if initial_mouse_offset == Vector2(0,0):
+			#initial_mouse_offset = event.position
+		position = event.global_position - initial_mouse_offset
+	if event is InputEventMouseButton and event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			click_start_time = Time.get_ticks_msec()
+			initial_mouse_offset = get_local_mouse_position()
+			print("pressed")
+		if event.is_released():
+			if Time.get_ticks_msec() - click_start_time <= 500 or position.y < -size.y * 0.8:
+				on_played.emit()
+				print("played!")
+				
+			click_start_time = 0
+			initial_mouse_offset = Vector2(0,0)
+			print("released")
+	
+	pass # Replace with function body.
