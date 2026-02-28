9fa6d7dd92f39121e1d86c04216cd07f8fc7dd3c
MasaMesgho <james_sam_whittingham@outlook.com>
2026-02-28 21:09:00 +0000

    main game loop

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -4,6 +4,7 @@ extends Node
 @onready var PillarAnimator: AnimationPlayer = %BoxAnimator
 @onready var CameraAnimator: AnimationPlayer = %PlayerAnimator
 @onready var Loot: Control = %Loot
+@onready var main_hud: Control = $MainHUD
 @onready var card_controller: Node = $MainHUD/CardController
 
 var enemy: Creature
@@ -13,6 +14,7 @@ var player: Creature = Player.new()
 func _ready() -> void:
 	CameraAnimator.play("ZoomOnBox")
 	LootCutsceneStart()
+	enemy = Enemy.Cerberous.new()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,15 +33,29 @@ func LootPhase():
 	var Option2: Card = Loot.get_basic_card()
 	var Option3: Relic = Loot.get_relic()
 	
+func _on_end_turn():
+	main_hud.clear_hand()
+	card_controller.new_player_hand()
 	
-func main_loop():
-	enemy = Enemy.Cerberous.new()
-	card_controller.
-
+	player.take_damage(enemy.strength)
+	if player.health <= 0:
+		play_death_animation()
 		
+	else:
+		if enemy.health <= 0:
+			LootCutsceneStart()
+		main_hud.show_hand(card_controller.player_hand.deck_list)
+	
+func _on_card_play(played_card: Card):
+	played_card.play(enemy, player)
+	
+func play_death_animation():
+	print("dead, bleh.")
+
 func LootCutsceneEnd():
 	CameraAnimator.play_backwards("ZoomOnBox")	#PillarAnimator.play_backwards("Raise")
 	PillarAnimator.play_backwards("Close")
+	main_hud.visible = true
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
